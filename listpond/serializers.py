7c4abf0521c7405ba92eca9b9f80b30318d8c7c6
hsmgowtham <hsmgowtham@gmail.com>
2024-05-07 16:23:19 -0400

    close #20, #21, #22

--- listpond/serializers.py
+++ listpond/serializers.py
@@ -16,10 +16,7 @@ class ListLabelSerializer(serializers.ModelSerializer):
 
 class UserListsSerializer(serializers.ModelSerializer):
     created_by = serializers.CharField(read_only=True)
-    shared_with_users = serializers.CharField(
-    allow_blank=True, 
-    required=False
-)
+    shared_with_users = serializers.CharField(allow_blank=True, required=False)
 
     class Meta:
         model = UserLists
@@ -28,20 +25,21 @@ class UserListsSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["created_by"] = self.context["request"].user
         return super().create(validated_data)
-    
+
     def validate_shared_with_users(self, value):
         print(f"{value=}")
         if value:
-            shared_with_users_list = [int(user_id) for user_id in value.split(',') if user_id.isdigit()]
+            shared_with_users_list = [
+                int(user_id) for user_id in value.split(",") if user_id.isdigit()
+            ]
             return shared_with_users_list
         return value
-    
+
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         shared_users = instance.shared_with_users.all()
-        representation['shared_with_users'] = [user.username for user in shared_users]
+        representation["shared_with_users"] = [user.username for user in shared_users]
         return representation
-
 
 
 class ListShareSerializer(serializers.ModelSerializer):
